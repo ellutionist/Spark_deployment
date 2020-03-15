a6622eedae52cde1847cf59e3a0edb339a5b0d69
ellutionist <impc53802956@163.com>
2020-03-15 16:04:39 +1100

    3.15

--- Server.py
+++ Server.py
@@ -3,11 +3,9 @@ from fabric.runners import Result
 from paramiko import SSHException
 from invoke import Responder, UnexpectedExit
 from configparser import ConfigParser
-import os
-
 
 class Server:
-    __config_path = "./config.ini"
+    __config_path = "./conf/server.ini"
 
     def __init__(self, port: int):
         self.__port = port
@@ -22,17 +20,17 @@ class Server:
 
     def upload_profile(self):
         """
-        upload the profile that edited locally, if the environmental variables are not correct.
+        Upload the profile that edited locally, if the environmental variables are not correct.
         :return: None
         """
         if not self._check_profile():
             profile_path = self.__config["java"].get("profile_path")
 
-            pwd = self.__conn.run("pwd").stdout.strip()
+            pwd = self.__conn.run("pwd", hide=True).stdout.strip()
             self.__conn.put(profile_path, '{pwd}/profile'.format(pwd=pwd))
-            self.__conn.run('sudo chown root ~/profile', pty=True, watchers=[self.__sudopass])
-            self.__conn.run('sudo chmod 644 ~/profile', pty=True, watchers=[self.__sudopass])
-            self.__conn.run('sudo mv ~/profile /etc/profile', pty=True, watchers=[self.__sudopass])
+            self.__conn.run('sudo chown root ~/profile', pty=True, watchers=[self.__sudopass], hide=True)
+            self.__conn.run('sudo chmod 644 ~/profile', pty=True, watchers=[self.__sudopass], hide=True)
+            self.__conn.run('sudo mv ~/profile /etc/profile', pty=True, watchers=[self.__sudopass], hide=True)
             self.__conn.run('source /etc/profile')
 
             # check the profile again
@@ -45,18 +43,20 @@ class Server:
         :return: None
         """
         if not self._check_java():
+            print("Installing Java on port {}.".format(self.__port))
+
             java_tar_path = self.__config["java"].get("java_tar_path")
             JAVA_HOME = self.__config["java"].get("JAVA_HOME")
             java_folder_name = self.__config["java"].get("java_folder_name")
 
             self._ensure_directory("~/Downloads")
 
-            pwd = self.__conn.run("pwd").stdout.strip()
+            pwd = self.__conn.run("pwd", hide=True).stdout.strip()
             self.__conn.put(java_tar_path, "{pwd}/Downloads/jdk8.tar".format(pwd=pwd))
             self.__conn.run('cd ~/Downloads && tar -xf ~/Downloads/jdk8.tar')
-            self.__conn.run('sudo mv ~/Downloads/{folder_name} {JAVA_HOME}'.
-                            format(folder_name=java_folder_name, JAVA_HOME=JAVA_HOME),
-                            pty=True, watchers=[self.__sudopass])
+            self.__conn.run('sudo mv ~/Downloads/{folder_name} {JAVA_HOME}'.format(folder_name=java_folder_name,
+                                                                                   JAVA_HOME=JAVA_HOME),
+                            pty=True, watchers=[self.__sudopass], hide=True)
 
             # check java again
             if not self._check_java():
@@ -68,18 +68,20 @@ class Server:
         :return: None
         """
         if not self._check_spark():
+            print("Installing Spark on port {}.".format(self.__port))
             spark_tar_path = self.__config["spark"].get("spark_tar_path")
             SPARK_HOME = self.__config["spark"].get("SPARK_HOME")
             spark_folder_name = self.__config["spark"].get("spark_folder_name")
 
             self._ensure_directory("~/Downloads")
+            self._ensure_directory("~/opt/module")
 
-            pwd = self.__conn.run("pwd").stdout.strip()
+            pwd = self.__conn.run("pwd", hide=True).stdout.strip()
             self.__conn.put(spark_tar_path, "{pwd}/Downloads/spark.tar".format(pwd=pwd))
             self.__conn.run('cd ~/Downloads && tar -xf ~/Downloads/spark.tar')
-            self.__conn.run('sudo mv ~/Downloads/{folder_name} {SPARK_HOME}'.
-                            format(folder_name=spark_folder_name, SPARK_HOME=SPARK_HOME),
-                            pty=True, watchers=[self.__sudopass])
+            self.__conn.run('sudo mv ~/Downloads/{folder_name} {SPARK_HOME}'.format(folder_name=spark_folder_name,
+                                                                                    SPARK_HOME=SPARK_HOME),
+                            pty=True, watchers=[self.__sudopass], hide=True)
 
             # check spark again
             if not self._check_spark():
@@ -87,20 +89,21 @@ class Server:
 
     def _connect(self) -> Connection:
         """
-        Connect to the server using ssh. Try to log in with public key at first, if failed then set up public key
+        Connect to the server using ssh. Try to log in with public key at first, if fail then set up public key
         authentication.
         :return: fabric.Connection
         """
         private_key_path: str = self.__config["ssh"].get("private_key_path")
         try:  # try to connect with private key first
             conn = Connection(host="localhost", port=self.__port, user=self.__username,
-                              config={"key_filename": private_key_path})
-            conn.run("pwd")
-        except SSHException:  # if failed, connect with password, then set private key authentication.
+                              connect_kwargs={"key_filename": private_key_path})
+            conn.run("pwd", hide=True)
+        except Exception:
             conn = Connection(host="localhost", port=self.__port, user=self.__username,
-                              config={"password": self.__password})
-            conn.run("pwd")
+                              connect_kwargs={"password": self.__password})
+            conn.run("pwd", hide=True)
             self._set_ssh_authentication(conn)
+        print("Connected to port {}.".format(self.__port))
 
         return conn
 
@@ -110,11 +113,12 @@ class Server:
         :param conn: ssh connection from fabric
         :return: None
         """
+        print("Setting public key authentication on port {port}.".format(port=self.__port))
         public_key_path: str = self.__config["ssh"].get("public_key_path")
-        pwd = conn.run("pwd").stdout.strip()
+        pwd = conn.run("pwd", hide=True).stdout.strip()
 
         try:
-            conn.run("cd ~/.ssh")  # check the existence of ~/.ssh/
+            conn.run("cd ~/.ssh", hide=True)  # check the existence of ~/.ssh/
         except UnexpectedExit:  # if the .ssh does not exist, create it.
             conn.run("mkdir ~/.ssh")
             conn.run("chmod 700 ~/.ssh")
@@ -138,7 +142,7 @@ class Server:
         """
         JAVA_HOME = self.__config["java"].get("JAVA_HOME")
         try:
-            self.__conn.run("cd {JAVA_HOME}".format(JAVA_HOME=JAVA_HOME))  # check the directory of java
+            self.__conn.run("cd {JAVA_HOME}".format(JAVA_HOME=JAVA_HOME), hide=True)  # check the directory of java
             print("Java has already been installed on port {port}.".format(port=self.__port))
             return True
         except UnexpectedExit:
@@ -151,7 +155,7 @@ class Server:
         """
         SPARK_HOME = self.__config["spark"].get("SPARK_HOME")
         try:
-            self.__conn.run("cd {SPARK_HOME}".format(SPARK_HOME=SPARK_HOME))  # check the directory of spark
+            self.__conn.run("cd {SPARK_HOME}".format(SPARK_HOME=SPARK_HOME), hide=True)  # check the directory of spark
             print("Spark has already been installed on port {port}.".format(port=self.__port))
             return True
         except UnexpectedExit:
@@ -165,21 +169,25 @@ class Server:
         JAVA_HOME = self.__config["java"].get("JAVA_HOME")
         SPARK_HOME = self.__config["spark"].get("SPARK_HOME")
 
-        result_java: Result = self.__conn.run("source /etc/profile && echo $JAVA_HOME")
-        result_spark: Result = self.__conn.run("source /etc/profile && echo $SPARK_HOME")
+        result_java: Result = self.__conn.run("source /etc/profile && echo $JAVA_HOME", hide=True)
+        result_spark: Result = self.__conn.run("source /etc/profile && echo $SPARK_HOME", hide=True)
 
         return result_java.stdout.strip() == JAVA_HOME and result_spark.stdout.strip() == SPARK_HOME
 
     def _ensure_directory(self, path: str):
         """
-        Universal method to check whether a director exists and if not, create it.
+        Universal method to check whether a director exists and if not, create it (recursively).
         :param path: the specific path
         :return: None
         """
         try:
-            self.__conn.run("cd {path}".format(path=path))
+            self.__conn.run("cd {path}".format(path=path), hide=True)
         except UnexpectedExit:
+            path = path[:-1] if path[-1] == "/" else path  # remove the possible "/" at tail
+            higher_level = path[:-len(path.split("/")[-1])]
+            self._ensure_directory(higher_level)  # recursively call itself to ensure the higher level is created
             self.__conn.run("mkdir {path}".format(path=path))
+            print("mkdir {path} on port {port}.".format(path=path, port=self.__port))
 
 
 class JavaInstallationFailure(Exception):
@@ -198,4 +206,8 @@ class ProfileFailure(Exception):
 
 
 if __name__ == '__main__':
-    raise SparkInstallationFailure(10001)
+
+    slave2 = Server(10003)
+    slave2.upload_profile()
+    slave2.install_java()
+    slave2.install_spark()
