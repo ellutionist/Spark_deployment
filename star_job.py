85134653dae72c3c036221949fa63b6f081da143
ellutionist <impc53802956@163.com>
2020-03-30 10:31:34 +1100

    the entrance to start job

--- star_job.py
+++ star_job.py
@@ -6,9 +6,11 @@ from os import mkdir
 
 
 def main(job: str):
+    # connect to master
     with open("conf/master-port", "r") as f:
         master = Master(int(f.read()))
 
+    # connect to slaves
     with open("conf/slave-ports", "r") as f:
         ports: List[int] = [int(line) for line in f.readlines()]
         slaves: List[Server] = []
@@ -17,14 +19,18 @@ def main(job: str):
             slave = Server(port)
             slaves.append(slave)
 
+    # start monitor on each slave
     for slave in slaves:
-        slave.start_monitor(interval=0.1)
+        slave.start_monitor(interval=0.01)
 
+    # send command to master to start the job
     master.get_connection().run("source /etc/profile && cd $SPARK_HOME && " + job)
 
+    # job is done, stop monitors, the slaves will write data to their own disks
     for slave in slaves:
         slave.stop_monitor()
 
+    # collect data to "./monitor_data/"
     current_time = str(datetime.now())[:-7]
     folder_name = input("Please input the folder name (default: {current_time}):\n".format(current_time=current_time))
     folder_name = folder_name if folder_name else current_time
