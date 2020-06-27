24d0a20390fb69aeceecf0b3ca5503fcf22b5701
ellutionist <impc53802956@163.com>
2020-06-27 10:40:41 +1000

    fix path problem

--- sbin/start-setup.sh
+++ sbin/start-setup.sh
@@ -2,4 +2,4 @@ SHELL_FOLDER=$(cd "$(dirname "$0")";pwd)
 script_path=$SHELL_FOLDER/../scripts/set_up.py
 
 export PYTHONPATH=$PYTHONPATH:$SHELL_FOLDER/..
-python3 $script_path
+cd $SHELL_FOLDER/../ && python3 $script_path
